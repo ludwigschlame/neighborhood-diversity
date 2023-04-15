2b32df3510a19675b325dabf1a77b38fd5511889
Ludwig Schlame <ludwig.schlame@gmail.com>
2023-04-15 20:26:48 +0200

    Make Benchmarks More Consistent

--- benches/random_graphs.rs
+++ benches/random_graphs.rs
@@ -2,16 +2,23 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use neighborhood_diversity::*;
 
 fn nd_calc(c: &mut Criterion) {
-    let graph_2 = Graph::random_graph_nd_limited(1e2 as usize, 0.2, 20);
-
     c.bench_function("Naive ALgorithm (10^2)", |b| {
+        let graph_2 = Graph::random_graph_nd_limited(1e2 as usize, 0.5, 20);
         b.iter(|| calc_nd_classes(&graph_2, Options::naive()))
     });
 
+    c.bench_function("Limited Comparisons (10^2)", |b| {
+        let graph_2 = Graph::random_graph_nd_limited(1e2 as usize, 0.5, 20);
+        b.iter(|| calc_nd_classes(&graph_2, Options::new(false, true)))
+    });
+
     c.bench_function("Degree Filter (10^2)", |b| {
+        let graph_2 = Graph::random_graph_nd_limited(1e2 as usize, 0.5, 20);
         b.iter(|| calc_nd_classes(&graph_2, Options::new(true, false)))
     });
+
     c.bench_function("ALL (10^2)", |b| {
+        let graph_2 = Graph::random_graph_nd_limited(1e2 as usize, 0.5, 20);
         b.iter(|| calc_nd_classes(&graph_2, Options::optimized()))
     });
 }
