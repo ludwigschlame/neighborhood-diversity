16bfbad1cfc0827c11af120d9f79e5a2dd117167
Ludwig Schlame <ludwig.schlame@gmail.com>
2023-04-20 21:02:23 +0200

    Improve Benchmark Formatting

--- benches/random_graphs.rs
+++ benches/random_graphs.rs
@@ -2,28 +2,40 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use neighborhood_diversity::*;
 
 fn nd_calc(c: &mut Criterion) {
-    c.bench_function("Naive ALgorithm (10^2)", |b| {
-        let graph_2 = Graph::random_graph_nd_limited(1e2 as usize, 0.5, 20);
+    const VERTEX_COUNT: usize = 1e2 as usize;
+    const ND_LIMIT: usize = 20;
+    const DENSITY: f64 = 0.5;
+
+    println!("|-------------------|");
+    println!("|   Graph Details   |");
+    println!("|-------------------|");
+    println!("| Vertices | {: >6} |", format!("{:+e}", VERTEX_COUNT));
+    println!("| ND Limit | {: >6} |", format!("{:+e}", ND_LIMIT));
+    println!("| Density  | {: >6} |", DENSITY);
+    println!("|-------------------|");
+
+    c.bench_function("Naive ALgorithm", |b| {
+        let graph_2 = Graph::random_graph_nd_limited(VERTEX_COUNT, DENSITY, ND_LIMIT);
         b.iter(|| calc_nd_classes(&graph_2, Options::naive()))
     });
 
-    c.bench_function("Limited Comparisons (10^2)", |b| {
-        let graph_2 = Graph::random_graph_nd_limited(1e2 as usize, 0.5, 20);
+    c.bench_function("Limited Comparisons", |b| {
+        let graph_2 = Graph::random_graph_nd_limited(VERTEX_COUNT, DENSITY, ND_LIMIT);
         b.iter(|| calc_nd_classes(&graph_2, Options::new(false, true)))
     });
 
-    c.bench_function("Degree Filter (10^2)", |b| {
-        let graph_2 = Graph::random_graph_nd_limited(1e2 as usize, 0.5, 20);
+    c.bench_function("Degree Filter", |b| {
+        let graph_2 = Graph::random_graph_nd_limited(VERTEX_COUNT, DENSITY, ND_LIMIT);
         b.iter(|| calc_nd_classes(&graph_2, Options::new(true, false)))
     });
 
-    c.bench_function("All (10^2)", |b| {
-        let graph_2 = Graph::random_graph_nd_limited(1e2 as usize, 0.5, 20);
+    c.bench_function("Optimized", |b| {
+        let graph_2 = Graph::random_graph_nd_limited(VERTEX_COUNT, DENSITY, ND_LIMIT);
         b.iter(|| calc_nd_classes(&graph_2, Options::optimized()))
     });
 
-    c.bench_function("BTree (10^2)", |b| {
-        let graph_2 = Graph::random_graph_nd_limited(1e2 as usize, 0.5, 20);
+    c.bench_function("BTree", |b| {
+        let graph_2 = Graph::random_graph_nd_limited(VERTEX_COUNT, DENSITY, ND_LIMIT);
         b.iter(|| calc_nd_btree(&graph_2))
     });
 }
