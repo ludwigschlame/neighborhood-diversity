ludwigschlame/neighborhood-diversity

commit 8c7c3a7a63e3a3371e4356288773181389988cef

Make Fields of 'Graph' Private

src/lib.rs
@@ -1,6 +1,8 @@
 mod graph;
+mod graph_adj_list;
 
-pub use graph::Graph;
+// pub use graph::Graph;
+pub use graph_adj_list::Graph;
 
 use std::collections::BTreeMap;
 
@@ -34,18 +36,18 @@ impl Options {
 }
 
 pub fn calc_nd_classes(graph: &Graph, options: Options) -> Vec<Vec<usize>> {
-    let mut type_connectivity_graph = Graph::null_graph(graph.vertex_count);
+    let mut type_connectivity_graph = Graph::null_graph(graph.vertex_count());
 
     let mut degrees: Vec<usize> = Vec::new();
     if options.degree_filter {
         // collect degrees for all vertices
-        degrees = (0..graph.vertex_count)
+        degrees = (0..graph.vertex_count())
             .map(|vertex| graph.degree(vertex))
             .collect();
     }
 
-    for u in 0..graph.vertex_count {
-        for v in u..graph.vertex_count {
+    for u in 0..graph.vertex_count() {
+        for v in u..graph.vertex_count() {
             // only compare neighborhoods if vertices have same degree
             if options.degree_filter && degrees[u] != degrees[v] {
                 continue;
@@ -73,7 +75,7 @@ pub fn calc_nd_btree(graph: &Graph) -> Vec<Vec<usize>> {
     let mut cliques: BTreeMap<Vec<_>, usize> = BTreeMap::new();
     let mut independent_sets: BTreeMap<Vec<_>, usize> = BTreeMap::new();
 
-    for vertex in 0..graph.vertex_count {
+    for vertex in 0..graph.vertex_count() {
         // let mut clique_type: Vec<_> = graph
         //     .neighbors(vertex)
         //     .into_iter()
@@ -85,7 +87,7 @@ pub fn calc_nd_btree(graph: &Graph) -> Vec<Vec<usize>> {
         //     independent_set_type.remove(pos);
         // }
 
-        let mut clique_type: Vec<bool> = vec![false; graph.vertex_count];
+        let mut clique_type: Vec<bool> = vec![false; graph.vertex_count()];
         for neighbor in graph.neighbors(vertex) {
             clique_type[neighbor] = true;
         }

src/graph.rs
@@ -7,8 +7,8 @@ use std::collections::HashSet;
 #[derive(Debug)]
 #[cfg_attr(test, derive(PartialEq))]
 pub struct Graph {
-    pub vertex_count: usize,
-    pub adjacency_matrix: Vec<Vec<bool>>,
+    vertex_count: usize,
+    adjacency_matrix: Vec<Vec<bool>>,
 }
 
 impl Graph {
@@ -163,6 +163,11 @@ impl Graph {
         self.adjacency_matrix[u][v] = false;
         self.adjacency_matrix[v][u] = false;
         Ok(was_present)
+    }
+
+    // returns number of vertices in the graph
+    pub fn vertex_count(&self) -> usize {
+        self.vertex_count
     }
 
     // returns neighbors of given vertex

commit 8a3013c0b63e891a357217cffa05bbb71d8bc7a5

Fix Bug Introduced in 8c7c3a7

src/lib.rs
@@ -1,5 +1,4 @@
 mod graph;
-mod graph_adj_list;
 
 pub use graph::Graph;
 