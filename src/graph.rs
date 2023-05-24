ludwigschlame/neighborhood-diversity

commit 3de1d5d9320614283a15f09889338b6e4cbda373

Formatting

src/graph.rs
@@ -624,7 +624,7 @@ impl Graph {
 // first line contains number of vertices
 // following lines list one edge each
 // vertex indices are separated by a comma: u,v
-// ignores: empty lines; lines starting with '#' or '//'
+// ignores: empty lines; lines starting with '#', '//' or '%'
 impl std::str::FromStr for Graph {
     type Err = Box<dyn std::error::Error>;
 
@@ -633,13 +633,13 @@ impl std::str::FromStr for Graph {
         let mut relevant_lines = input
             .lines()
             .map(str::trim)
-            .filter(|line| !(line.is_empty() || line.starts_with('#') || line.starts_with("//")));
+            .filter(|line| !(line.is_empty() || line.starts_with('#') || line.starts_with('%')));
         // first relevant line should contain the number of vertices
         let vertex_count = relevant_lines
             .next()
             .ok_or("input does not contain graph data")?
             .parse()?;
-        let mut graph = Self::null_graph(vertex_count, Representation::AdjacencyList);
+        let mut graph = Self::null_graph(vertex_count, Representation::AdjacencyMatrix);
 
         // for each remaining line, tries to split once at comma
         // then tries to parse both sides as vertex indices

commit ef298a07492e7f34a70b0968e36712e53d155ef5

Fix Bug Introduced in 3de1d5d

src/graph.rs
@@ -630,10 +630,12 @@ impl std::str::FromStr for Graph {
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
         // filter out comments and empty lines
-        let mut relevant_lines = input
-            .lines()
-            .map(str::trim)
-            .filter(|line| !(line.is_empty() || line.starts_with('#') || line.starts_with('%')));
+        let mut relevant_lines = input.lines().map(str::trim).filter(|line| {
+            !(line.is_empty()
+                || line.starts_with('#')
+                || line.starts_with("//")
+                || line.starts_with('%'))
+        });
         // first relevant line should contain the number of vertices
         let vertex_count = relevant_lines
             .next()
@@ -706,10 +708,10 @@ mod tests {
 
         let graph_truth = Graph {
             vertex_count: 3,
-            representation: InternalRepresentation::AdjacencyList(vec![
-                vec![1, 2],
-                vec![0],
-                vec![0],
+            representation: InternalRepresentation::AdjacencyMatrix(vec![
+                vec![false, true, true],
+                vec![true, false, false],
+                vec![true, false, false],
             ]),
         };
 
@@ -732,10 +734,10 @@ mod tests {
 
         let graph_truth = Graph {
             vertex_count: 3,
-            representation: InternalRepresentation::AdjacencyList(vec![
-                vec![1, 2],
-                vec![0],
-                vec![0],
+            representation: InternalRepresentation::AdjacencyMatrix(vec![
+                vec![false, true, true],
+                vec![true, false, false],
+                vec![true, false, false],
             ]),
         };
 