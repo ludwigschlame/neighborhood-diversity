7d2022eba90178203a39dcc39306b43f3e6aed71
Ludwig Schlame <ludwig.schlame@gmail.com>
2023-08-07 11:05:40 +0200

    Formatting

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,5 +2,7 @@ pub use crate::cotree::Cotree;
 pub use crate::graph::Graph;
 pub use crate::graph::Representation::*;
 pub use crate::md_tree::MDTree;
-pub use crate::Options;
-pub use crate::{calc_nd_btree, calc_nd_classes};
+pub use crate::Optimizations;
+pub use crate::{
+    calc_nd_btree, calc_nd_btree_concurrent, calc_nd_btree_concurrent_old, calc_nd_classes,
+};
